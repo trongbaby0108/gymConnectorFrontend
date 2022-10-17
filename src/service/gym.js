98a6de21350c7de215047d990496957a5544a9e5
DESKTOP-R98UOPN\trong <tronghandsome1111@gmail.com>
2022-10-17 13:00:12 +0700

    upload

--- src/service/gym.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { domain } from "../api/api";
-
-
-export const getGyms = () => {
-    axios.get(domain + "home/getGym").then((response) => {
-        return response.data;
-    });
-};
