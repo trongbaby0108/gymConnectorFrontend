6242f80867939dc3d764d144b38093295a505cec
Khaang0506 <nhk06052001@gmail.com>
2022-10-27 22:57:07 +0700

    Add admin template

--- src/components/Admin/css/tailwind.config.js
+++ src/components/Admin/css/tailwind.config.js
@@ -1,7 +1,10 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       boxShadow: {
@@ -12,6 +15,9 @@ module.exports = {
       },
       outline: {
         blue: '2px solid rgba(0, 112, 244, 0.5)',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
       },
       fontSize: {
         xs: ['0.75rem', { lineHeight: '1.5' }],
@@ -47,50 +53,9 @@ module.exports = {
         60: '60',
       },
     },
-    fontFamily: {
-      primary: 'Inter',
-      secondary: 'Open Sans',
-    },
-    container: {
-      padding: {
-        DEFAULT: '0',
-      },
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1038px',
-    },
-    extend: {
-      colors: {
-        primary: {
-          100: '#a5ffce',
-          200: '#4ae290',
-          300: '25c870',
-        },
-        neutral: {
-          100: '#ffffff',
-          200: '#dedee3',
-          300: '#9797a1',
-          400: '#595962',
-          500: '#131316',
-        },
-        page: '#fcfcff',
-      },
-      backgroundImage: {
-        banner: "url('/src/assets/img/banner/bg.png')",
-        faq: "url('/src/assets/img/faq/bg.svg')",
-      },
-      backgroundSize: {
-        auto: 'auto',
-        cover: 'cover',
-        contain: 'contain',
-        custom: '100%',
-      },
-    },
   },
   plugins: [
+    // eslint-disable-next-line global-require
     require('@tailwindcss/forms'),
     // add custom variant for expanding sidebar
     plugin(({ addVariant, e }) => {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,3 @@
-import Cart from "../components/pages/Cart/Cart";
 import GymDetail from "../components/pages/Gyms/GymDetail";
 import Gyms from "../components/pages/Gyms/Gyms";
 import Home from "../components/pages/Home";
@@ -6,7 +5,7 @@ import Login from "../components/pages/Login";
 import Register from "../components/pages/Signup";
 import Trainers from "../components/pages/Trainers/Trainers";
 import TrainerDetail from "../components/pages/Trainers/TrainerDetail";
-
+import Dashboard from "../components/Admin/pages/Dashboard";
 
 export const routes = {
     publicRoutes: [
@@ -18,6 +17,7 @@ export const routes = {
         { path: '/trainers', component: Trainers },
         { path: '/gyms/:id', component: GymDetail },
         { path: '/trainers/:id', component: TrainerDetail },
-        { path: '/cart', component: Cart },
+        //{ path: '/cart', component: Cart },
+        { path: '/admin', component: Dashboard }
     ]
 }
