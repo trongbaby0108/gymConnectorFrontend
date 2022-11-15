4dfb503f2baeead1e6c67f2300c86207607b122d
Khaang0506 <nhk06052001@gmail.com>
2022-11-15 22:58:00 +0700

    new upload image and preview, apply formik to form in admin template, create trainerInfo

--- src/components/pages/User/UserInfo.jsx
+++ src/components/pages/User/UserInfo.jsx
@@ -1,10 +1,72 @@
+import { useFormik } from "formik";
 import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../../Features/Footer";
 import Header from "../../Features/Header";
 import UserIcn from "../../pages/User/user_info.svg";
+import * as Yup from "yup";
+import PreviewImage from "../../Features/PreviewImage";
+import axios from "axios";
 
 const UserInfo = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      address: "",
+      phone: "",
+      email: "",
+      avatar: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required("Không được bỏ trống mục này")
+        .min(4, "Phải nhiều hơn 4 ký hoặc hơn"),
+      email: Yup.string()
+        .required("Không được bỏ trống mục này")
+        .matches(
+          /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+          "Vui lòng điền địa chỉ email đúng định dạng"
+        ),
+      phone: Yup.string()
+        .required("Không được bỏ trống mục này")
+        .matches(
+          /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
+          "Vui lòng điền đúng định dạng số điện thoại"
+        ),
+      address: Yup.string().required("Không được bỏ trống mục này"),
+      avatar: Yup.mixed()
+        .required("Không được bỏ trống mục này")
+        .test(
+          "FILE_SIZE",
+          "Ảnh quá lớn",
+          (value) => value && value.size < 1280 * 1280
+        )
+        .test(
+          "FILE_TYPE",
+          "Không tồn tại hoặc không đúng định dạng",
+          (value) =>
+            value &&
+            ["image/png", "image/jpg", "image/jpeg"].includes(value.type)
+        ),
+    }),
+    onSubmit: async () => {
+      console.log(formik.values);
+      const { avatar } = formik.values;
+      const formData = new FormData();
+      try {
+        formData.append("file", avatar);
+        formData.append("upload_preset", "loghpveg");
+        const res = await axios.post(
+          "https://api.cloudinary.com/v1_1/dwjck5c9f/image/upload",
+          formData
+        );
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
   return (
     <div className="max-w-[1920px] mx-auto bg-page overflow-hidden relative">
       <Header />
@@ -25,20 +87,30 @@ const UserInfo = () => {
         </div>
         <div data-aos="fade-up" data-aos-delay="300">
           <div className="max-w-full flex flex-col justify-center items-center">
-            <div className="grid md:grid-cols-2 md:gap-16">
+            <div
+              className="grid md:grid-cols-2 md:gap-16"
+              onSubmit={formik.handleSubmit}
+            >
               <form>
                 <div className="relative z-0 mb-6 w-full group">
                   <input
                     type="email"
-                    name="floating_email"
-                    id="floating_email"
+                    name="email"
+                    id="email"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     required=""
                     autoComplete="off"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
                   />
+                  {formik.errors.email && (
+                    <p className="max-w-full text-xs text-red-500">
+                      {formik.errors.email}
+                    </p>
+                  )}
                   <label
-                    for="floating_email"
+                    htmlFor="email"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Địa chỉ email
@@ -47,49 +119,47 @@ const UserInfo = () => {
                 <div className="relative z-0 mb-6 w-full group">
                   <input
                     type="text"
-                    name="floating_first_name"
-                    id="floating_first_name"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" "
-                    required=""
-                  />
-                  <label
-                    for="floating_first_name"
-                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    Họ
-                  </label>
-                </div>
-                <div className="relative z-0 mb-6 w-full group">
-                  <input
-                    type="text"
-                    name="floating_last_name"
-                    id="floating_last_name"
+                    name="name"
+                    id="name"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     required=""
                     autoComplete="off"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
                   />
+                  {formik.errors.name && (
+                    <p className="max-w-full text-xs text-red-500">
+                      {formik.errors.name}
+                    </p>
+                  )}
                   <label
-                    for="floating_last_name"
+                    htmlFor="name"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
-                    Tên
+                    Họ & Tên
                   </label>
                 </div>
                 <div className="relative z-0 mb-6 w-full group">
                   <input
                     type="tel"
                     pattern="[0-9]{10}"
-                    name="floating_phone"
-                    id="floating_phone"
+                    name="phone"
+                    id="phone"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     required=""
                     autoComplete="off"
+                    value={formik.values.phone}
+                    onChange={formik.handleChange}
                   />
+                  {formik.errors.phone && (
+                    <p className="max-w-full text-xs text-red-500">
+                      {formik.errors.phone}
+                    </p>
+                  )}
                   <label
-                    for="floating_phone"
+                    htmlFor="phone"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Số điện thoại
@@ -98,15 +168,22 @@ const UserInfo = () => {
                 <div className="relative z-0 mb-6 w-full group">
                   <input
                     type="text"
-                    name="floating_company"
-                    id="floating_company"
+                    name="address"
+                    id="address"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     required=""
                     autoComplete="off"
+                    value={formik.values.address}
+                    onChange={formik.handleChange}
                   />
+                  {formik.errors.address && (
+                    <p className="max-w-full text-xs text-red-500">
+                      {formik.errors.address}
+                    </p>
+                  )}
                   <label
-                    for="floating_company"
+                    htmlFor="address"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Địa chỉ
@@ -131,29 +208,35 @@ const UserInfo = () => {
                 </div>
               </form>
               <div className="spacing-15">
-                <img
-                  class="w-80 h-80 rounded-full"
-                  src="https://scr.vn/wp-content/uploads/2020/07/avt-cute.jpg"
-                  alt="description"
-                />
-
+                {formik.values.avatar && (
+                  <PreviewImage file={formik.values.avatar} />
+                )}
                 <label
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  for="file_input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  htmlFor="avatar"
                 >
                   Đăng tải hình ảnh
                 </label>
                 <input
-                  class="block h-7 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                  className="block h-7 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                   aria-describedby="file_input_help"
-                  id="file_input"
+                  id="avatar"
+                  name="avatar"
                   type="file"
+                  onChange={(e) =>
+                    formik.setFieldValue("avatar", e.target.files[0])
+                  }
                 />
+                {formik.errors.avatar && (
+                  <p className="max-w-full text-xs text-red-500">
+                    {formik.errors.avatar}
+                  </p>
+                )}
                 <p
-                  class="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                  className="mt-1 text-sm text-gray-500 dark:text-gray-300"
                   id="file_input_help"
                 >
-                  Tải lên file có đuôi .PNG, .JPG.
+                  Tải lên file có đuôi .PNG, .JPG, .JPEG
                 </p>
               </div>
             </div>
